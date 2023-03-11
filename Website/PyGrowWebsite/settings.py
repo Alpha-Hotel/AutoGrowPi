2d93775d84c4597bc321b33c635345d1fd6010a9
Alpha-Hotel <workmail2159@gmail.com>
2023-03-11 09:40:25 -0700

    tied docker port to host, added template

--- Website/PyGrowWebsite/settings.py
+++ Website/PyGrowWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^pdawv30!($yke0lvff$k6l(sjpd1drzu@m%aj+*oj*3we(^i%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'PyGrowWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/website/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
