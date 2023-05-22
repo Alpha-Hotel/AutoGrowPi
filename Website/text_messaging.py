dabbd68ef2b228ee64c1720eb3cbb061a4dd64fc
Alpha-Hotel <workmail2159@gmail.com>
2023-05-22 11:54:26 -0600

    update secrets

--- Website/text_messaging.py
+++ Website/text_messaging.py
@@ -1,8 +1,9 @@
 import smtplib
+from os import environ as e
 # https://testingonprod.com/2021/10/24/how-to-send-text-messages-with-python-for-free/
 def test_message(phone_number, carrier):
     recipient=phone_number+carrier
-    auth = ("ahparadox2159@gmail.com", "tkxezoxojqckfnnz")
+    auth = ("ahparadox2159@gmail.com", e['APPLICATION_KEY'])
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(auth[0], auth[1])
